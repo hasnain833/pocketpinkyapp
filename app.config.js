23705eed1b228adad0408f10a244e6a406b95938
Hasnain Aftab <hasnainaftab87@gmail.com>
2026-02-24 17:00:00 +0500

    Build Fix

--- app.config.js
+++ app.config.js
@@ -5,9 +5,12 @@ const expoConfig = appJson.expo;
 
 module.exports = {
   ...expoConfig,
-  owner: 'hasnain833',
+  owner: 'hasnain87',
   extra: {
     ...expoConfig.extra,
+    eas: {
+      projectId: "81f578e5-64e4-4c95-9923-1fd6c10ac26d"
+    },
     supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL,
     supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
   },
